48cd04d9f36d1ce9097ac7f2bd98504bc3fc64b9
Inácio Moraes <inacio.moraes@sga.pucminas.br>
2018-10-31 17:44:48 -0300

    procfile added

--- gyms/controller.go
+++ gyms/controller.go
@@ -6,7 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	)
+)
+
 var Gyms []Academia
 
 //Controller ...
@@ -15,22 +16,22 @@ type Controller struct {
 
 func Init() {
 	log.Print("Reading data...")
-	data, err:=ioutil.ReadFile("data/output.json")
-	err= json.Unmarshal(data,&Gyms)
-	if err!=nil{
+	data, err := ioutil.ReadFile("data/output.json")
+	err = json.Unmarshal(data, &Gyms)
+	if err != nil {
 		log.Fatal(err)
 	}
 	log.Print("Done!")
 }
+
 // GetQuiz GET /
 func (c *Controller) GetAcademias(w http.ResponseWriter, r *http.Request) {
-	data, err:=json.Marshal(Gyms)
-	print("bang")
+	data, err := json.Marshal(Gyms)
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(422)
 		if err := json.NewEncoder(w).Encode("Error Marshaling json"); err != nil {
-			log.Printf("Error GetAcademia: %+v \n", errors.Wrap(err,"json failed to encode"))
+			log.Printf("Error GetAcademia: %+v \n", errors.Wrap(err, "json failed to encode"))
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -43,4 +44,3 @@ func (c *Controller) GetAcademias(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 	return
 }
-
