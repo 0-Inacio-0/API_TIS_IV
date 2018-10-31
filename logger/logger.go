cee9f8c85c4ef2d434c9e4c82bff36388094cdb4
Inácio Moraes <inacio.moraes@sga.pucminas.br>
2018-10-31 20:01:32 -0300

    logger now has better details on request.

--- logger/logger.go
+++ logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"time"
 )
@@ -9,9 +10,16 @@ import (
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+		ip, port, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			log.Printf("userip: %q is not IP:port", r.RemoteAddr)
+		}
+
 		inner.ServeHTTP(w, r)
 		log.Printf(
-			"%s\t%s\t%s\t%s",
+			"Request:\tIP:%s\tPORT:%s\t%s\t%s\t%s\t%s",
+			ip,
+			port,
 			r.Method,
 			r.RequestURI,
 			name,
